97171a52bc81d56866b06dfefed14a30f7b81e3c
kristbg <kristbg@gmail.com>
2023-12-08 13:52:54 -0500

    added repo links to project cards

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
-# React + Vite
+# Chris Godinho's React Portfolio Page
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+## Description
 
-Currently, two official plugins are available:
+A single-page portfolio application using React capabilities.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+![A render of the app in execution](/public/images/readme_render.jpg)
+
+## Installation
+
+No installation is necessary. The web application can be accessed [here](<https://main--dulcet-kringle-086ab6.netlify.app/>).
+
+## How can I Contribute?
+
+This is a one-off project that's not being supported any longer. Feel free, however, to clone this repo (but make sure to credit the original creators).
+
+## Credits
+
+This application was created by [Chris Godinho](<https://github.com/chris-godinho>).
+
+A large part of the research for this project was done through the websites Stack Overflow (<https://stackoverflow.com/>), W3 Schools (<https://www.w3schools.com/>) and ChatGPT (<https://chat.openai.com/>), and as such small sections of code were adapted from examples and suggestions offered in those sites.
+
+## License
+
+This project is licensed under the [MIT](/LICENSE) License.
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -7,6 +7,7 @@ export default function Portfolio() {
       imageUrl: '/tunemuse.jpeg',
       title: 'Tune-Muse',
       link: 'https://tune-muse-team.github.io/tunemuse/',
+      repoLink: 'https://github.com/tune-muse-team/tunemuse',
       description: 'An AI-curated music application that provides song recommendations based on a set of parameters and text queries entered by the user.'
     },
     {
@@ -14,6 +15,7 @@ export default function Portfolio() {
       imageUrl: '/travis.jpg',
       title: 'TravIS',
       link: 'https://travis231017-9762a24c277b.herokuapp.com/',
+      repoLink: 'https://github.com/UofTTeam2/TravIS',
       description: 'TravIS (or TRAVel Interface System) is a useful tool for planning your next trip and discovering new horizons to explore.',
     },
     {
@@ -21,6 +23,7 @@ export default function Portfolio() {
       imageUrl: '/js_quiz.jpeg',
       title: 'JavaScript Quiz',
       link: 'https://kristbg.github.io/2023-08-03-javascript-quiz-game/',
+      repoLink: 'https://github.com/chris-godinho/2023-08-03-javascript-quiz-game',
       description: 'An interactive quiz game on basic JavaScript knowledge.',
     },
     {
@@ -28,6 +31,7 @@ export default function Portfolio() {
       imageUrl: '/techblog.jpeg',
       title: "Chris' Tech Thoughts",
       link: 'https://peaceful-dawn-48719-8b024e4e358e.herokuapp.com/',
+      repoLink: 'https://github.com/chris-godinho/2023-10-19-MVC-tech-blog',
       description: "A blog where developers can publish their posts as well as comment on other developers'.",
     },
     {
@@ -35,6 +39,7 @@ export default function Portfolio() {
       imageUrl: '/weatherforecast.jpeg',
       title: 'Weather Forecast',
       link: 'https://kristbg.github.io/2023-08-21-weather-forecast/',
+      repoLink: 'https://github.com/chris-godinho/2023-08-21-weather-forecast',
       description: 'A simple application for obtaining the weather forecast for a given city, obtaining data through the OpenWeather API.',
     },
     {
@@ -42,6 +47,7 @@ export default function Portfolio() {
       imageUrl: '/notetaker.jpeg',
       title: 'Note Taker',
       link: 'https://immense-brook-26564-3b2b5bd98ff7.herokuapp.com/',
+      repoLink: 'https://github.com/chris-godinho/2023-09-27-note-taker',
       description: 'A simple application for taking and saving notes ',
     },
   ];
@@ -49,7 +55,7 @@ export default function Portfolio() {
   return (
     <div className="flex-page">
       {cardsData.map((card) => (
-        <Project key={card.id} imageUrl={card.imageUrl} title={card.title} link={card.link} description={card.description}/>
+        <Project key={card.id} imageUrl={card.imageUrl} title={card.title} link={card.link} repoLink={card.repoLink} description={card.description}/>
       ))}
     </div>
   );
